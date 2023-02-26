e235808c99ab0351358f960da2532ea27678f6b5
Tanner <tmartzdc@gmail.com>
2023-02-26 11:11:17 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class BinaryNumber:
     def __repr__(self):
         return('decimal=%d binary=%s' % (self.decimal_val, ''.join(self.binary_vec)))
     
-
 ## Implement multiplication functions here. Note that you will have to
 ## ensure that x, y are appropriately sized binary vectors for a
 ## divide and conquer approach.
@@ -45,23 +44,52 @@ def pad(x,y):
     return x,y
 
 def quadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+    # 1. obtian binary values of x and y calling them xvec and yvec
+    xvec = x.binary_vec
+    yvec = y.binary_vec
+    
+    # 2. pad xvec and yvec so lengths are equal for proper splitting and recombination
+    xvec, yvec = pad(xvec, yvec)
 
+    # 3. base case returning product of x and y if both <=1
+    if x.decimal_val <=1 and y.decimal_val <=1:
+        return BinaryNumber(x.decimal_val*y.decimal_val)
 
+    # 4. split xvec and yvec into two halves
+    x_left, x_right = split_number(xvec)
+    y_left, y_right = split_number(yvec)
+
+    # 5. implement apply quadratic multiply formula
+    # formula:
+    # left = 2^n * x_left * y_left
+    # mid = 2^(n /2) * (x_left * y_right + x_right * y_left)
+    # right = x_right * y_right
+
+    left = quadratic_multiply(x_left, y_left)
+    mid_left = quadratic_multiply(x_left, y_right)
+    mid_right = quadratic_multiply(x_right, y_left)
+
+    mid = BinaryNumber(mid_left.decimal_val + mid_right.decimal_val)
+
+    # 6. use bitshift for 2^n and 2^(n/2) multiplications
+    mid = bit_shift(mid, len(xvec)//2) 
+    left = bit_shift(left, len(xvec))
+    right = quadratic_multiply(x_right, y_right)
+
+    # 7. sum the 3 decimal values for final answer to return
+    return BinaryNumber(left.decimal_val + mid.decimal_val + right.decimal_val)
 
 ## Feel free to add your own tests here.
 def test_multiply():
-    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)).decimal_val == 2*2
     
+    # added tests fully functional
+    assert quadratic_multiply(BinaryNumber(4), BinaryNumber(6)).decimal_val == 4*6
+    assert quadratic_multiply(BinaryNumber(2), BinaryNumber(7)).decimal_val == 2*7
     
 def time_multiply(x, y, f):
     start = time.time()
     # multiply two numbers x, y using function f
     return (time.time() - start)*1000
 
-
-    
-    
-
+test_multiply()
